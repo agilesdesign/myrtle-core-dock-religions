9e6b99cfc87f8787de9e176c2dd08e077fea9129
Justin Seliga <jseliga@agilesdesign.com>
2017-04-07 07:40:11 -0400

    fix path for config files

--- src/Myrtle/Core/Docks/ReligionsDock.php
+++ src/Myrtle/Core/Docks/ReligionsDock.php
@@ -42,8 +42,8 @@ class ReligionsDock extends Dock
     public function configPaths()
     {
         return [
-            'docks.' . self::class => dirname(__DIR__, 2) . '/config/docks/religions.php',
-            'abilities' => dirname(__DIR__, 2) . '/config/abilities.php',
+            'docks.' . self::class => dirname(__DIR__, 3) . '/config/docks/religions.php',
+            'abilities' => dirname(__DIR__, 3) . '/config/abilities.php',
         ];
     }
 
@@ -55,7 +55,7 @@ class ReligionsDock extends Dock
     public function migrationPaths()
     {
         return [
-            dirname(__DIR__, 2) . '/database/migrations',
+            dirname(__DIR__, 3) . '/database/migrations',
         ];
     }
 
@@ -67,7 +67,7 @@ class ReligionsDock extends Dock
     public function routes()
     {
         return [
-            'admin' => dirname(__DIR__, 2) . '/routes/admin.php',
+            'admin' => dirname(__DIR__, 3) . '/routes/admin.php',
         ];
     }
 }
